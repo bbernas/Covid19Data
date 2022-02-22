73a0b47a73594cd23b7de9a31b07472999beae02
Léa Le Large <lea.le-large@insa-lyon>
2022-02-22 12:08:55 -0600

    stacked dataframe

--- covid19.py
+++ covid19.py
@@ -16,19 +16,16 @@ county_pop = pd.read_csv('https://raw.githubusercontent.com/bbernas/Covid19Data/
 print(county_pop.head())
 deaths = pd.read_csv('https://raw.githubusercontent.com/bbernas/Covid19Data/master/covid_deaths_usafacts.csv')
 print(deaths.head())
+confirmed_copy=confirmed.copy()
 
-#%% Reorganize the DataFrame
-index = confirmed.iloc[:,[0,1,2,3]]
-print(index)
-countyFIPS = confirmed.iloc[:,[0]]
-countyName = confirmed.iloc[:,[1]]
-StateName = confirmed.iloc[:,[2]]
-StateFIPS = confirmed.iloc[:,[3]]
-
-#%% Multi-Index
-index = pd.MultiIndex.from_frame(df=confirmed,names=["countyFIPS","County Name","State","StateFIPS"])
+#%% Set the index
+index = confirmed.set_index(["countyFIPS","County Name","State","StateFIPS"])
 print(index)
 
-#%% Tests
-print(confirmed.index)
-print(confirmed.columns)
+#%% Stack
+stacked_confirmed = index.stack()
+print(stacked_confirmed)
+
+#%%
+total_cases = stacked_confirmed.to_frame()
+print(total_cases)
